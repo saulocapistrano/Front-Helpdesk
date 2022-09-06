717b03bd5053fbf46bcf635c7f4960b1137898d0
Matheus Abreu <matheus.abreu.magalhaes@gmail.com>
2022-09-06 13:12:53 -0300

    componente NAV criado

--- front-helpdesk/helpdesk/src/app/app.module.ts
+++ front-helpdesk/helpdesk/src/app/app.module.ts
@@ -25,11 +25,13 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
+import { NavComponent } from './components/nav/nav.component';
 
-
+// componentes utilizados no projeto
 @NgModule({
   declarations: [
     AppComponent,
+    NavComponent,
   
   ],
   imports: [
