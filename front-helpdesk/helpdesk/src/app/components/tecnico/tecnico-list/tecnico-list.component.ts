a2c7007aca527c7d3a01792cf68b31168bbe858d
Matheus Abreu <matheus.abreu.magalhaes@gmail.com>
2022-09-06 16:36:48 -0300

    Criação da paginação do Técnico

--- front-helpdesk/helpdesk/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ front-helpdesk/helpdesk/src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tecnico-list.component.css']
 })
 export class TecnicoListComponent implements OnInit {
+ 
+  ELEMENT_DATA: Tecnico[] = [ 
+    {
+    id: 1,
+    nome: "José da Silva",
+    cpf: '00088799816',
+    email: 'josesilva@gmail.com',
+    senha: '123',
+    perfils:['0'],
+    dataCriacao: '06/09/2022'
+  }
+  ]
+  
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol','acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
