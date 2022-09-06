96d1daf81d8673796717edb16c7c410618df5a71
Matheus Abreu <matheus.abreu.magalhaes@gmail.com>
2022-09-06 15:46:41 -0300

    Criação do componente TECNICO-LIST

--- front-helpdesk/helpdesk/src/app/app.module.ts
+++ front-helpdesk/helpdesk/src/app/app.module.ts
@@ -28,6 +28,7 @@ import { MatCardModule } from '@angular/material/card';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 // componentes utilizados no projeto
 @NgModule({
@@ -36,6 +37,7 @@ import { HeaderComponent } from './components/header/header.component';
     NavComponent,
     HomeComponent,
     HeaderComponent,
+    TecnicoListComponent,
   
   ],
   imports: [
