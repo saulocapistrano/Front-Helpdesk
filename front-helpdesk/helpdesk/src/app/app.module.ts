2bec4644439813c8050e19b533a8b65793dafccd
Matheus Abreu <matheus.abreu.magalhaes@gmail.com>
2022-09-08 15:17:14 -0300

    Serviço de MENSSAGENS implementados

--- front-helpdesk/helpdesk/src/app/app.module.ts
+++ front-helpdesk/helpdesk/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { LoginComponent } from './components/login/login.component';
+import { ToastrModule } from 'ngx-toastr';
 
 // componentes utilizados no projeto
 @NgModule({
@@ -67,7 +68,11 @@ import { LoginComponent } from './components/login/login.component';
     MatIconModule,
     MatListModule,
     MatCardModule,
-    
+    ToastrModule.forRoot({
+      timeOut:4000,
+      closeButton: true,
+      progressBar: true
+    })
    
   ],
   providers: [],
--- front-helpdesk/helpdesk/src/app/components/login/login.component.ts
+++ front-helpdesk/helpdesk/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { Credenciais } from 'src/app/models/credenciais';
  
 
@@ -16,9 +17,16 @@ export class LoginComponent implements OnInit {
 
   email = new FormControl(null, Validators.email);
   senha = new FormControl(null, Validators.minLength(3));
-  constructor() { }
+  
+  constructor(private toast: ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  logar(){
+this.toast.error('Usuário ou Senha inválidos','login')
+this.creds.email='';
+this.creds.senha='';
   }
 
   validaCampos (): boolean{
