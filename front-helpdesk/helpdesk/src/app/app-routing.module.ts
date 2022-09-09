0f70b9d9208744c5f38bb36de166bd9b589b1cca
Matheus Abreu <matheus.abreu.magalhaes@gmail.com>
2022-09-09 12:42:44 -0300

    Rota para COMPONENT-TECNICO criada

--- front-helpdesk/helpdesk/src/app/app-routing.module.ts
+++ front-helpdesk/helpdesk/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
+import { TecnicoCreateComponent } from './components/tecnico/tecnico-create/tecnico-create.component';
 import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 import { AlthService } from './services/alth.service';
 
@@ -11,7 +12,9 @@ const routes: Routes = [
   {
     path:'',component: NavComponent, canActivate:[AlthService] , children:[
     {path:'home', component: HomeComponent},
-    {path:'tecnico', component: TecnicoListComponent}
+
+    {path:'tecnico', component: TecnicoListComponent},
+    {path:'tecnico/create', component: TecnicoCreateComponent}
 ]}
 
 ];
